7e4e54849a48f3bfae8e0ca786622a9324c619b2
= <=>
2020-03-28 17:44:17 +0800

    地图定位功能

--- app/src/main/java/mapcluster/smile/com/bdmapcluster/MainActivity.java
+++ app/src/main/java/mapcluster/smile/com/bdmapcluster/MainActivity.java
@@ -3,6 +3,8 @@ package mapcluster.smile.com.bdmapcluster;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -25,18 +27,29 @@ import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.PolylineOptions;
 import com.baidu.mapapi.model.LatLng;
 import com.baidu.mapapi.utils.DistanceUtil;
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 import mapcluster.smile.com.bdmapcluster.base.BaseActivity;
+import mapcluster.smile.com.bdmapcluster.model.response.GetAlarmListResponse;
 import mapcluster.smile.com.bdmapcluster.util.AndroidLocationUtils;
+import mapcluster.smile.com.bdmapcluster.util.PeopleMapTrackUtil;
+import mapcluster.smile.com.bdmapcluster.util.ShowPictureUtil;
 import mapcluster.smile.com.bdmapcluster.util.ShowToast;
 import mapcluster.smile.com.bdmapcluster.util.UIUtils;
+import mapcluster.smile.com.bdmapcluster.widget.badge_view.BadgeViewPro;
+import mapcluster.smile.com.bdmapcluster.widget.circle_imageview.SWImageView;
 
 public class MainActivity extends BaseActivity {
 
@@ -57,7 +70,7 @@ public class MainActivity extends BaseActivity {
     /**
      * 百度地图聚合点管理类
      */
-    private ClusterManager<BaiDuMapMarkerItem> mClusterManager;
+    private ClusterManager<BaiDuMapClusterItem> mClusterManager;
 
     private String locationAddress = "";
     private LatLng locationLatlng;//保存定位接口回调结果的经纬度，用于添加一个固定不变的标记(marker)
@@ -68,6 +81,18 @@ public class MainActivity extends BaseActivity {
     private float mapCurrentZoom = 15.0f;//实时记录地图层级 默认15
     private LatLng rightTopLatlng, leftBottomLatlng;
     private HashMap<Integer, Integer> mapZoomScale = new HashMap<>();// 地图缩放层级比例尺对应的距离
+
+    private ArrayList<BaiDuMapClusterItem> mapMarkerItemList = new ArrayList<>();
+
+    /**
+     * 记录显示marker时，使用的ImageView和数据对象MarkerBean
+     */
+    private HashMap<ImageView, MarkerBean> hashMapMarkerView = new HashMap<>();
+
+    /**
+     * 图片加载完成次数，无论成功与否
+     */
+    private int hasShowPictureFinishCount;
 
     @Override
     protected int getLayoutId() {
@@ -109,17 +134,17 @@ public class MainActivity extends BaseActivity {
     private void initClusterManager() {
         // 定义点聚合管理类ClusterManager
         mClusterManager = new ClusterManager<>(mContext, mBaiduMap);
-        mClusterManager.setOnClusterClickListener(new ClusterManager.OnClusterClickListener<BaiDuMapMarkerItem>() {
+        mClusterManager.setOnClusterClickListener(new ClusterManager.OnClusterClickListener<BaiDuMapClusterItem>() {
             @Override
-            public boolean onClusterClick(Cluster<BaiDuMapMarkerItem> cluster) {
+            public boolean onClusterClick(Cluster<BaiDuMapClusterItem> cluster) {
                 ShowToast.showToast(mContext, "有" + cluster.getSize() + "个点");
                 Log.e(TAG, TAG + "---onClusterClick 发生了");
                 return false;
             }
         });
-        mClusterManager.setOnClusterItemClickListener(new ClusterManager.OnClusterItemClickListener<BaiDuMapMarkerItem>() {
+        mClusterManager.setOnClusterItemClickListener(new ClusterManager.OnClusterItemClickListener<BaiDuMapClusterItem>() {
             @Override
-            public boolean onClusterItemClick(BaiDuMapMarkerItem item) {
+            public boolean onClusterItemClick(BaiDuMapClusterItem item) {
 
                 return false;
             }
@@ -221,6 +246,20 @@ public class MainActivity extends BaseActivity {
         }, 210);
     }
 
+    /**
+     * 重新添加聚合点marker
+     */
+    private void reloadClusterMarker() {
+        Log.e(TAG, "开始聚合了 mapMarkerItemList = " + mapMarkerItemList.size());
+        mClusterManager.clearItems();//清除所有的items
+        //        mClusterManager.getMarkerCollection().clear();
+        //        mClusterManager.getClusterMarkerCollection().clear();
+        mClusterManager.addItems(mapMarkerItemList);
+        // 算法计算聚合，并显示
+        // 类似于通知地图刷新聚合点marker
+        mClusterManager.cluster();
+    }
+
     @OnClick({R.id.image_location})
     public void onViewClicked(View view) {
         if (R.id.image_location == view.getId()) {
@@ -245,14 +284,143 @@ public class MainActivity extends BaseActivity {
             //必须在地图加载完后才能去设置，否则设置无效
             View zoomView = mMapView.getChildAt(2);//获取缩放控件
             int x = mMapView.getWidth() - zoomView.getWidth() - UIUtils.dp2px(mContext, 10);
-            int y = mMapView.getHeight() - zoomView.getHeight() - UIUtils.dp2px(mContext, 10);
+            int y = mMapView.getHeight() - zoomView.getHeight() - UIUtils.dp2px(mContext, 20);
+            // 设置缩放控件的位置
             mMapView.setZoomControlsPosition(new Point(x, y));
-            y = mMapView.getHeight() - imageLocation.getHeight() - UIUtils.dp2px(mContext, 10);
-            UIUtils.setViewLayout(imageLocation, UIUtils.dp2px(mContext, 6), y);
+
+            x = mMapView.getWidth() - zoomView.getWidth() - UIUtils.dp2px(mContext, 13);
+            y = y - imageLocation.getHeight() - UIUtils.dp2px(mContext, 10);
+            UIUtils.setViewLayout(imageLocation, x, y);
             Log.e(TAG, "地图加载完成 width = " + zoomView.getWidth() + ", height = " + zoomView.getHeight()
                     + ", " + imageLocation.getHeight());
+
+            List<GetAlarmListResponse.DataBean.ListBean> dataBeanList = PeopleMapTrackUtil.getAlarmListResponse().getData().getList();
+            BaiduMapManager.getInstance().goToTargetLocation(mBaiduMap
+                    , dataBeanList.get(dataBeanList.size() - dataBeanList.size() / 2).getBaiDuLatLng()
+                    , 16);
+
+            BaiDuMapClusterItem mapMarkerItem;
+            List<LatLng> markerList = new ArrayList<>();
+
+            GetAlarmListResponse.DataBean.ListBean listBean;
+            for (int i = 0; i < dataBeanList.size(); i++) {
+                listBean = dataBeanList.get(i);
+
+                mapMarkerItem = new BaiDuMapClusterItem(listBean.getBaiDuLatLng(), listBean.getUrl());
+                mapMarkerItemList.add(mapMarkerItem);
+                markerList.add(listBean.getBaiDuLatLng());
+
+                // 构建Marker图标对应的view
+                View markerView = LayoutInflater.from(mContext).inflate(R.layout.view_map_marker_people, null);
+                // 小红点
+                BadgeViewPro tvPeopleCount = markerView.findViewById(R.id.tv_people_count);
+                // 圆形头像
+                SWImageView imagePeopleHeader = markerView.findViewById(R.id.image_people_header);
+
+                if (i == 0) {
+                    // 终点
+                    tvPeopleCount.setText("1");
+                    imagePeopleHeader.setBorder_color(ContextCompat.getColor(getApplicationContext(), R.color.color_last_marker));
+                } else if (i == dataBeanList.size() - 1) {
+                    // 起点
+                    tvPeopleCount.setShape(3);
+                    tvPeopleCount.setText("99+");
+                    imagePeopleHeader.setBorder_color(ContextCompat.getColor(getApplicationContext(), R.color.color_first_marker));
+                } else {
+                    Random random = new Random();
+                    tvPeopleCount.setText(String.valueOf(random.nextInt(50 ) + 50));
+                    imagePeopleHeader.setBorder_color(ContextCompat.getColor(getApplicationContext(), R.color.color_period_marker));
+                }
+                tvPeopleCount.setTextBgShape();
+
+                // 构建Marker需要的数据对象
+                MarkerBean markerBean = new MarkerBean(listBean.getBaiDuLatLng(), markerView);
+                hashMapMarkerView.put(imagePeopleHeader, markerBean);
+
+                // 加载单个marker图片的网络回调监听
+                ShowPictureUtil.setShowPictureListener(new ShowPictureUtil.ShowPictureListener() {
+                    @Override
+                    public void showPictureResult(Drawable resourceDrawable, ImageView imageView) {
+                        // 图片加载结果监听回调
+                        hasShowPictureFinishCount++;
+                        // 构建Marker图标
+                        BitmapDescriptor markerBitmap = BitmapDescriptorFactory.fromView(hashMapMarkerView.get(imageView).getMarkerView());
+                        // 构建MarkerOption，用于在地图上添加Marker
+                        OverlayOptions overlayOptions = new MarkerOptions()
+                                .position(hashMapMarkerView.get(imageView).getLatlng())
+                                .icon(markerBitmap)
+                                .zIndex(8)
+                                .draggable(true);
+                        // 3、开始给地图添加marker，这里是一个一个添加，也可以选择批量添加
+                        mBaiduMap.addOverlay(overlayOptions);
+
+                        if (hasShowPictureFinishCount >= markerList.size()) {
+                            // 所有图片加载完成
+                            // 4、设置marker连线的参数
+                            startPolyline(markerList);
+                        }
+                    }
+                });
+                // 开始加载图片
+                ShowPictureUtil.showImage(MainActivity.this, listBean.getUrl(), imagePeopleHeader);
+            }
         }
     };
+
+    private void addAllMarker(List<GetAlarmListResponse.DataBean.ListBean> beanList) {
+        // 1、起点位置
+        LatLng startLatlng = beanList.get(0).getBaiDuLatLng();
+        // 构建Marker图标对应的view
+        View startMarkerView = LayoutInflater.from(mContext).inflate(R.layout.view_map_marker_people, null);
+        TextView startTvPeopleCount = startMarkerView.findViewById(R.id.tv_people_count);
+        ImageView startImagePeopleHeader = startMarkerView.findViewById(R.id.image_people_header);
+        ShowPictureUtil.showImage(MainActivity.this, beanList.get(0).getUrl(), startImagePeopleHeader);
+        // 构建Marker图标
+        BitmapDescriptor startMarkerBitmap = BitmapDescriptorFactory.fromView(startMarkerView);
+        // 构建MarkerOption，用于在地图上添加Marker
+        OverlayOptions option = new MarkerOptions()
+                .position(startLatlng)
+                .icon(startMarkerBitmap).zIndex(8).draggable(true);
+
+
+        // 2、终点位置
+        LatLng endLatlng = beanList.get(beanList.size() - 1).getBaiDuLatLng();
+        // 构建Marker图标对应的view
+        View endMarkerView = LayoutInflater.from(mContext).inflate(R.layout.view_map_marker_people, null);
+        TextView endTvPeopleCount = endMarkerView.findViewById(R.id.tv_people_count);
+        ImageView endImagePeopleHeader = endMarkerView.findViewById(R.id.image_people_header);
+        ShowPictureUtil.showImage(MainActivity.this, beanList.get(beanList.size() - 1).getUrl(), endImagePeopleHeader);
+        // 构建Marker图标
+        BitmapDescriptor endMarkerBitmap = BitmapDescriptorFactory.fromView(endMarkerView);
+        // 构建MarkerOption，用于在地图上添加Marker
+        OverlayOptions option1 = new MarkerOptions()
+                .position(endLatlng)
+                .icon(endMarkerBitmap).zIndex(8).draggable(true);
+
+        // 3、在地图上添加Marker，并显示
+        List<OverlayOptions> overlay = new ArrayList<>();
+        overlay.add(option);
+        overlay.add(option1);
+        mBaiduMap.addOverlays(overlay);
+
+        // 4、设置连线的参数
+        startPolyline(new ArrayList<>());
+    }
+
+    /**
+     * 设置marker连线的参数
+     *
+     * @param markerList marker点位集合
+     */
+    private void startPolyline(List<LatLng> markerList) {
+        PolylineOptions polylineOptions = new PolylineOptions()
+                .width(5)
+                .dottedLine(false)//false：实线， true：虚线
+                .color(ContextCompat.getColor(getApplicationContext(), R.color.color_marker_line))
+                .points(markerList);// 添加点位集合数据
+        // 绘制连接marker坐标点的线
+        mBaiduMap.addOverlay(polylineOptions);
+    }
 
     private BDLocationListener mBaiDuMapLocationListener = new BDLocationListener() {
 
@@ -306,16 +474,46 @@ public class MainActivity extends BaseActivity {
     };
 
     /**
-     * 每个Marker点，包含Marker点坐标以及图标  方便聚合功能使用
+     * marker数据bean对象
      */
-    private class BaiDuMapMarkerItem implements ClusterItem {
+    private class MarkerBean {
+        private LatLng latlng;
+        private View markerView;
+
+        public MarkerBean(LatLng latlng, View markerView) {
+            this.latlng = latlng;
+            this.markerView = markerView;
+        }
+
+        public LatLng getLatlng() {
+            return latlng;
+        }
+
+        public void setLatlng(LatLng latlng) {
+            this.latlng = latlng;
+        }
+
+        public View getMarkerView() {
+            return markerView;
+        }
+
+        public void setMarkerView(View markerView) {
+            this.markerView = markerView;
+        }
+    }
+
+    /**
+     * 每个聚合的点，包含点坐标以及图标  方便聚合功能使用
+     */
+    private class BaiDuMapClusterItem implements ClusterItem {
         private LatLng latlng;
         private int markerType;
         private String num;
         private String imageHeaderUrl;
 
-        public BaiDuMapMarkerItem(LatLng latlng) {
+        public BaiDuMapClusterItem(LatLng latlng, String imageHeaderUrl) {
             this.latlng = latlng;
+            this.imageHeaderUrl = imageHeaderUrl;
         }
 
         public LatLng getLatlng() {
